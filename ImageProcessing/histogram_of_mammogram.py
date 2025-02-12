d6ee936a077f7ab8fdffc01a2a9df1788737b4f2
AggelosMargkas <39811197+AggelosMargkas@users.noreply.github.com>
2025-02-12 23:03:22 +0200

    Update histogram_of_mammogram.py

--- ImageProcessing/histogram_of_mammogram.py
+++ ImageProcessing/histogram_of_mammogram.py
@@ -2,13 +2,13 @@ import matplotlib.pyplot as plt  # importing matplotlib
 
 # reads image data
 img_normal = plt.imread(
-    r'C:\Users\junio\Desktop\Thesis\CBIS_DDSM_Image_Processing\manifest-1667995631662\CBIS-DDSM\Calc-Test_P_00077_LEFT_CC\anotherimage.png')
+    r'PATH\CBIS_DDSM_Image_Processing\manifest-1667995631662\CBIS-DDSM\Calc-Test_P_00077_LEFT_CC\anotherimage.png')
 
 # plt.imshow(img, cmap='gray')
 # plt.show()
 
 img_cropped = plt.imread(
-    r"C:\Users\junio\Desktop\Thesis\CBIS_DDSM_Image_Processing\outputImages\cropped_image2.png")
+    r"PATH\CBIS_DDSM_Image_Processing\outputImages\cropped_image2.png")
 
 
 histogramm_of__normal = plt.hist(img_normal.ravel(), bins=256, range=(0.0, 1.0),
